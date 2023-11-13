91805bc26bcb3394a6eab5d759e78deec708eda9
Eugene Litvin <eugene.litvin@interactiv4.com>
2023-11-14 00:25:27 +0200

    refactoring

--- visitor_bot.py
+++ visitor_bot.py
@@ -1,128 +1,119 @@
 import pyautogui as pg
+from pyautogui import ImageNotFoundException
 from time import sleep
 from env import env
-from random import randint
-from pathlib import Path
+from random import randint, uniform
 
 
 class VisitorBot:
 
     """
-    Set time counters
+    Set target page
     """
-
     def __init__(self):
-        self.counter = 0
-        self.wait_until = 0
+        pg.useImageNotFoundException()
         self.target_page = env['target_page'].rstrip('/')
-
-    def get_file(self, file_name: str) -> str:
-        path = Path(__file__).with_name(file_name)
-        return str(path.absolute())
 
     """
     Run general script
     """
-
     def run(self) -> None:
-        if self.open_browser() is False:
-            print(f"Tried to find browser on screen by icon "
-                  f"{Path(__file__).with_name(env['browser_icon'])} "
-                  f"but no success. please make sure you use correct image of icon")
-            return
+        pg.getWindowsWithTitle("Chrome")[0].activate()
         sleep(3)
         pg.moveTo(500, 500)
         pg.hotkey('ctrl', 't')
         search = set(env['search'])
         while len(search) > 0:
             self.search(search.pop())
-            pg.keyDown('end')
-            sleep(5)
+            if self.is_loaded(30) is False:
+                continue
             for i in range(0, randint(1, int(env['visits_limit']) + 1)):
-                self.visit_page()
-                sleep(1)
-            self.visit_page(is_website=True)
-
-    """
-    find next link on search page, visit site, scroll and go back
-    """
-
-    def visit_page(self, is_website=False):
-        pg.hotkey('ctrl', 'f')
-        if is_website is not False:
-            pg.write(self.target_page)
-        else:
-            pg.write('http')
-        pg.hotkey('ctrl', 'enter')
-        if is_website is True:
-            sleep(5)
-            if pg.locateCenterOnScreen(self.get_file(env['search_page_marker'])) is not None:
+                self.visit_random_page()
+                sleep(uniform(0.8, 1.1))
+            for i in range(0, 20):
+                pg.keyDown('pgdn')
+                sleep(uniform(0.3, 0.5))
+            if self.visit_target_page() is True:
                 return
-        # checking do we left search page
-        while pg.locateCenterOnScreen(self.get_file(env['search_page_marker'])) is not None:
-            sleep(1)
-        self.wait(init=True)
-        if is_website is False:
-            while self.wait() is False:
-                pg.scroll(randint(-20, -1))
-        else:
-            self.rand_wait(init=True)
-            while self.rand_wait() is False:
-                pg.scroll(randint(-30, 30))
-        pg.hotkey('alt', 'left')
 
     """
-    Open browser based on icon
+    visit random page from keywords search
     """
-
-    def open_browser(self) -> bool:
-        start = pg.locateCenterOnScreen(
-            self.get_file(env['browser_icon'])
+    def visit_random_page(self) -> None:
+        pg.hotkey('ctrl', 'f')
+        pg.write('http', 0.1)
+        _iter = randint(1, 10)
+        while _iter > 0:
+            pg.press("enter")
+            iter -= 1
+        pg.hotkey('ctrl', 'enter')
+        if self.is_loaded(30) is False:
+            pg.hotkey('alt', 'left')
+            return
+        wait = self.wait(
+            wait_from=int(env['visit_time_from']),
+            wait_to=int(env['visit_time_to']),
+            step_from=0.8,
+            step_to=1.1
         )
-        if start is None:
+        while next(wait) is False:
+            pg.press("pgdn")
+        pg.hotkey('alt', 'left')
+        return
+
+    """
+    attempt to find target page
+    """
+    def visit_target_page(self) -> bool:
+        pg.hotkey('ctrl', 'f')
+        pg.write(self.target_page, 0.1)
+        pg.hotkey('ctrl', 'enter')
+        if self.is_loaded() is False:
             return False
-        pg.moveTo(start)
-        pg.click()
+        wait = self.wait(
+            wait_from=int(env['target_page_visit_time_from']),
+            wait_to=int(env['target_page_visit_time_to']),
+            step_from=1,
+            step_to=int(env['max_rand_scroll_delay'])
+        )
+        while next(wait) is False:
+            pg.press(("pgdn", "pgup")[randint(0, 1)])
+        pg.hotkey('alt', 'left')
         return True
 
     """
     focus of search bar and input keyword 
     """
-
     def search(self, search: str) -> None:
         pg.hotkey('ctrl', 'k')
-        pg.write(search)
+        pg.write(search, uniform(0.1, 0.3))
         pg.hotkey('enter')
 
     """
     wait specified time
     """
+    def wait(self, wait_from, wait_to, step_from, step_to) -> bool:
+        counter = 0
+        wait_until = randint(wait_from, wait_to)
+        sleep_time = uniform(step_from, step_to)
+        while counter < wait_until - sleep_time:
+            sleep_time = uniform(step_from, step_to)
+            sleep(sleep_time)
+            counter += sleep_time
+            yield False
+        yield True
 
-    def wait(self, init=False):
-        if init is True:
-            self.counter = 0
-            self.wait_until = randint(
-                int(env['visit_time_from']),
-                int(env['visit_time_to'])
-            )
-            return False
-        sleep(1)
-        self.counter += 1
-        if self.counter == self.wait_until:
-            return True
-        return False
-
-    def rand_wait(self, init=False):
-        if init is True:
-            self.counter = 0
-            self.wait_until = randint(
-                int(env['target_page_visit_time_from']),
-                int(env['target_page_visit_time_to'])
-            )
-            return False
-        sleep_time = randint(1, env['max_rand_scroll_delay'])
-        sleep(sleep_time)
-        self.counter += sleep_time
-        if self.counter >= self.wait_until - env['max_rand_scroll_delay']:
+    """
+    check is page loaded
+    """
+    def is_loaded(self, max_wait=5) -> bool:
+        img = pg.screenshot(region=(100, 100, 300, 300))
+        try:
+            wait = 0
+            while wait < max_wait:
+                pg.locateOnScreen(img)
+                sleep(uniform(0.8, 1.1))
+                wait += 1
+        except ImageNotFoundException:
             return True
         return False
